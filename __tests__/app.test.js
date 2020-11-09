09d3d52fc7f69a9e952518db3662df953de9e097
Brian McPhillips <B.McPhillips89@gmail.com>
2020-11-08 20:55:54 -0800

    app controller hooked up to app.js test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,24 @@ const app = require('../lib/app');
 
 describe('Full-Stack-BE routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('creates a state via post', () => {
+    return request(app)
+      .post('/api/v1/states')
+      .send({
+        stateName: 'California',
+        electoralVotes: 55,
+        winner: 'Joe Biden'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          stateName: 'California',
+          electoralVotes: 55,
+          winner: 'Joe Biden'
+        });
+      });
   });
 });
