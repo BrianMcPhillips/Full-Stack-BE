17f550a6d2d6dc0591d2fd2e5761398bb1ee4199
Brian McPhillips <B.McPhillips89@gmail.com>
2020-11-09 00:30:11 -0800

    find method and get route added with test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const State = require('../lib/models/stateModel');
 
 describe('Full-Stack-BE routes', () => {
   beforeEach(() => {
@@ -23,6 +24,20 @@ describe('Full-Stack-BE routes', () => {
           electoralVotes: 55,
           winner: 'Joe Biden'
         });
+      });
+  });
+
+  it('finds all states via GET', async() => {
+    const states = await Promise.all([
+      State.insert({ stateName: 'Arizona', electoralVotes: 11, winner: 'Joe Biden' }),
+      State.insert({ stateName: 'Oregon', electoralVotes: 7, winner: 'Joe Biden' }),
+      State.insert({ stateName: 'Washington', electoralVotes: 12, winner: 'Joe Biden' }),
+    ]);
+
+    return request(app)
+      .get('/api/v1/states')
+      .then(res => {
+        expect(res.body).toEqual(expect.arrayContaining(states));
       });
   });
 });
